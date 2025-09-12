36e0d1a1ab448f2119464d6486796dd610265ce8
Developershubh00 <developershubh00@gmail.com>
2025-09-12 15:03:48 +0530

    changes in mac

--- backend/settings.py
+++ backend/settings.py
@@ -66,7 +66,7 @@ MIDDLEWARE = [
 
 
 
-CORS_ALLOWED_ORIGINS = config('CORS_ALLOWED_ORIGINS', default='backend-dju9.onrender.com,http://localhost:5173,http://localhost:5174,http://127.0.0.1:5173,http://127.0.0.1:5174').split(',')
+CORS_ALLOWED_ORIGINS = config('CORS_ALLOWED_ORIGINS', default='https://backend-dju9.onrender.com,http://localhost:5173,http://localhost:5174,http://127.0.0.1:5173,http://127.0.0.1:5174').split(',')
 
 # Additional CORS settings for production
 CORS_ALLOW_CREDENTIALS = True

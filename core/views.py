e143b09f686e263a7510c42f69af01da474ff7e4
Developershubh00 <developershubh00@gmail.com>
2025-11-07 17:08:50 +0530

    database

--- core/views.py
+++ core/views.py
@@ -1711,7 +1711,7 @@ class ForgotPasswordAPIView(APIView):
         uid = urlsafe_base64_encode(force_bytes(user.pk))
         
         # Create reset link
-        frontend_url = getattr(settings, 'FRONTEND_URL', 'http://localhost:5173')
+        frontend_url = getattr(settings, 'FRONTEND_URL' )
         reset_link = f"{frontend_url}/reset-password?token={token}&uid={uid}"
         
         user_name = user.first_name if user.first_name else user.username

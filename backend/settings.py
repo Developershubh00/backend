7e4ce55ba1855a22790c9d6b0cf4e5a71acecdc7
Developershubh00 <developershubh00@gmail.com>
2025-11-07 11:03:58 +0530

    CORS

--- backend/settings.py
+++ backend/settings.py
@@ -65,7 +65,16 @@ MIDDLEWARE = [
 
 
 
-CORS_ALLOWED_ORIGINS = config('CORS_ALLOWED_ORIGINS', default='https://backend-dju9.onrender.com,http://127.0.0.1:8000,http://127.0.0.1:5174').split(',')
+# With this FIXED code:
+CORS_ALLOWED_ORIGINS = [
+    'https://backend-dju9.onrender.com',
+    'http://localhost:5173',
+    'http://localhost:3000',
+    'https://believersconsultancy.com',
+    'https://www.believersconsultancy.com',
+    'http://127.0.0.1:8000',
+    'http://127.0.0.1:5174',
+]
 
 # Additional CORS settings for production
 CORS_ALLOW_CREDENTIALS = True
